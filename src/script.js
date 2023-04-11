8f39c4ccf2337e187c0081a549e7e9507e292f4e
sydney-jovian <126050345+sydney-jovian@users.noreply.github.com>
2023-04-11 14:45:23 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -9,7 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
       const convertedValue = currentValue
         .replace("×", "*")
         .replace("÷", "/")
-        .replace('%', '*0.01');
+        .replace('%', '*0.01')
+        .replace('sin', 'Math.sin')
+        .replace('cos', 'Math.cos')
+        .replace('ln', 'Math.log')
+        .replace('π', 'Math.PI')
+        .replace('log', 'Math.log10')
+        .replace('e', 'Math.E')
+        .replace('tan', 'Math.tan')
+        .replace('√', 'Math.sqrt');
       
       console.log('convertedValue:', convertedValue)
       const result = eval(convertedValue);
@@ -21,16 +29,24 @@ document.addEventListener("DOMContentLoaded", function () {
       const button = buttons[i];
       button.addEventListener('click', function() {
         const value = button.innerText;
-  
-        if (value == "AC") {
-          currentValue = "";
-          display.value = currentValue;
-        } else if(value == "=") {
-          evaluateResult();
-        } else {
-          currentValue += value;
-          display.value = currentValue;
+
+        try {
+            if (value == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+              } else if(value == "=") {
+                evaluateResult();
+              } else {
+                currentValue += value;
+                display.value = currentValue;
+              }
+        } catch (error) {
+            console.error(error);
+            currentValue = "ERROR";
+            display.value = currentValue;
         }
+
+        
       })
       
     }
